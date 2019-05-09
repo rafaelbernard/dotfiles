38e693f6eee69d351a443dd339bf4ed2b03915a7
Rafael Araujo <rafael.araujo@endel.com.br>
2019-05-09 00:48:03 -0300

    Add: platforms

--- install.sh
+++ install.sh
@@ -3,9 +3,39 @@ set -x
 
 echo "Installing dotfiles"
 
-BACKDIR="$HOME/.dotfiles-backup"
+# =========================
+# PLATFORM
+# =========================
 
-#if [[ -f $BACKDIR ]]
+#!/bin/bash
+
+platform_type=`uname -m`
+unamestr=`uname`
+platform='unknown'
+if [[ "$unamestr" == 'Linux' ]]; then
+    platform='linux'
+    if [[ -e "/etc/arch-release" ]]; then
+	platform='arch'
+    fi
+elif [[ "$unamestr" == 'armv7' ]]; then
+    platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    platform='macos'
+    install() {
+      if [[ -e "/usr/local/bin/$1" ]]; then
+        brew upgrade $1
+      else
+        brew install $1
+      fi
+    }
+fi
+export platform_type
+export platform
+export hostname=`hostname -s`
+
+backup_dir="$HOME/.dotfiles-backup"
+
+#if [[ -f $backup_dir ]]
 mkdir $BACKDIR
 
 # zsh
