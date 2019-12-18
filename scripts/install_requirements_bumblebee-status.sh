eeda30de153668727b348cab6ffd6a83c9215356
Rafael Bernard Araújo <rafael.araujo@initd.info>
2019-12-18 16:00:21 -0300

    Update requirements for bumblebee status

--- scripts/install_requirements_bumblebee-status.sh
+++ scripts/install_requirements_bumblebee-status.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# dependencies
+# TODO: enable other distributions
+sudo apt-get install -y python-psutil python-netifaces dnf pactl
+
 # downloard bumblebee-status at opt
 
 # change owner
 
 # link to /usr/bin/local
 
-# install python-psutil python-netifaces
