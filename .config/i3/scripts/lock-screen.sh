a68b2838be2a06c279dd7edd4a70c3b73b6566ce
Rafael Bernard Araújo <rafael.araujo@initd.info>
2019-12-30 10:49:41 -0300

    i3 lock screen - use actual wallpaper

--- .config/i3/scripts/lock-screen.sh
+++ .config/i3/scripts/lock-screen.sh
@@ -1,13 +1,28 @@
 #!/bin/sh -e
 
-# take a screenshot
-scrot -o /tmp/screen_shot.png
+wallpaper=$(readlink ~/.cache/wallpaper)
+#walpaper=''
+lockfile=$wallpaper
 
-# pixelit
-mogrify -scale 10% -scale 1000% /tmp/screen_shot.png
+if [ "$wallpaper" = "" ]; then
+        # take a screenshot
+        scrot -o /tmp/screen_shot.png
+        
+        # pixelit
+        mogrify -scale 10% -scale 1000% /tmp/screen_shot.png
+
+        lockfile='/tmp/screen_shot.png'
+else
+        screen_resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
+        converted='/tmp/wallpaper.png'
+        convert $wallpaper -background black -gravity center -resize "$screen_resolution" $converted
+        lockfile=$converted
+fi
+
 
 # lock screen displaying the image
-i3lock -i /tmp/screen_shot.png
+#i3lock -i /tmp/screen_shot.png
+i3lock --color=000000 -i $lockfile
 
 # Turn the screen off after a delay
-sleep 60; pgrep i3lock && xset dpms force off
+sleep 120; pgrep i3lock && xset dpms force off
