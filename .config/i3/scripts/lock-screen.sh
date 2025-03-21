4966a76c86a2927e4998a42885a2d4f69f84aa81
Rafael Bernard Araújo <rafael.araujo@initd.info>
2025-03-21 17:59:59 +1300

    OK

--- .config/i3/scripts/lock-screen.sh
+++ .config/i3/scripts/lock-screen.sh
@@ -12,29 +12,30 @@ if ! command -v xdpyinfo &> /dev/null ; then
     exit
 fi
 
+
 wallpaper=$(readlink ~/.cache/wallpaper)
 lockfile=$wallpaper
 
 if [ "$wallpaper" = "" ]; then
         # take a screenshot
-        scrot -o /tmp/screen_shot.png >> $log_file
+        scrot -o /tmp/screen_shot.png
         
         # pixelit
-        mogrify -scale 10% -scale 1000% /tmp/screen_shot.png >> $log_file
+        mogrify -scale 10% -scale 1000% /tmp/screen_shot.png
 
         lockfile='/tmp/screen_shot.png'
 else
         screen_resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
         converted='/tmp/wallpaper.png'
-        magick "$wallpaper" -background black -gravity center -resize "$screen_resolution" $converted >> $log_file
+        magick "$wallpaper" -background black -gravity center -resize "$screen_resolution" $converted
         lockfile=$converted
 fi
 
 
 # lock screen displaying the image
 # xss-lock so that the screen is locked *before* suspension
-xss-lock --transfer-sleep-lock -- i3lock --color=000000 -i $lockfile >> $log_file
+xss-lock --transfer-sleep-lock -- i3lock --color=000000 -i $lockfile
 
-i3lock --color=000000 -i $lockfile >> $log_file
+i3lock --color=000000 -i $lockfile
 # Turn the screen off after a delay
 sleep 30; pgrep i3lock && xset dpms force off
